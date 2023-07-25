2da17a83e1ffa58858b817a502ee3ad8c8d04c7a
denisKolodii <kolodiy83@gmail.com>
2023-07-25 20:19:28 +0300

    spring

---
+++ src/main/java/spring_introduction/Dog.java
@@ -0,0 +1,32 @@
+package spring_introduction;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog implements Pet{
+//    private String name;
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
+    @Override
+    public void say() {
+        System.out.println("Gav-Gav");
+    }
+
+    public void init() {
+        System.out.println("Class Dog : init method");
+    }
+
+    public void destroy() {
+        System.out.println("Class Dog : destroy method");
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+}
---
+++ src/main/java/spring_introduction/Person.java
@@ -0,0 +1,56 @@
+package spring_introduction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
+public class Person {
+//    @Autowired
+//    @Qualifier("catBean")
+    private Pet pet;
+    @Value("${person.surname}")
+    private String surname;
+    @Value("${person.age}")
+    private int age;
+
+    @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
+        System.out.println("Person Bean is created ");
+        this.pet = pet;
+    }
+//    public Person() {
+//        System.out.println("Person Bean is created ");
+//    }
+
+//    @Autowired
+//    @Qualifier("dog")
+    public void setPet(Pet pet) {
+        System.out.println("Class Person set pet ");
+        this.pet = pet;
+    }
+
+    public void callYourPet() {
+        System.out.println("Hello, my lovely Pet!");
+        pet.say();
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person set age");
+        this.age = age;
+    }
+}
